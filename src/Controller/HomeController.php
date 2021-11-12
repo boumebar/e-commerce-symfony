751e542918ceebf287f275d5a5d79417c731a4c5
boumediene <boume13@hotmail.com>
2021-11-12 23:15:43 +0100

    add relations entities

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,9 +14,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(ProductRepository $productRepository)
     {
-
-        return $this->render('home.html.twig');
+        $products = $productRepository->findBy([], [], 3);
+        return $this->render('home.html.twig', ['products' => $products]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -20,18 +21,31 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+
         $faker = Factory::create("fr_FR");
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \FakerEcommerce\Ecommerce($faker));
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
+            $category->setName($faker->company())
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $manager->persist($category);
 
-        for ($p = 0; $p < 100; $p++) {
-            $product = new Product;
-            $product->setName($faker->mobilePhones())
-                ->setPrice($faker->price())
-                ->setSlug(strtolower($this->slugger->slug($product->getName())));
-            $manager->persist($product);
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
+                $product = new Product;
+                $product->setName($faker->mobilePhones())
+                    ->setPrice($faker->price())
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
+                $manager->persist($product);
+            }
         }
+
+
 
         $manager->flush();
     }
