4dd651be8a4a820dd0c530f62f2c27765c4d18d6
boumediene <boume13@hotmail.com>
2021-11-13 01:35:42 +0100

    add link

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['slug' => $slug]);
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas ");
+        }
+        return $this->render('product/category.html.twig', [
+            "category"  => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show($slug, $category_slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+
+        if ($category_slug != $product->getCategory()->getSlug()) {
+            throw $this->createNotFoundException("Cette catégorie n'existe pas !");
+        }
+
+        if (!$product) {
+            throw $this->createNotFoundException("Ce produit n'existe pas !");
+        }
+        return $this->render('product/show.html.twig', [
+            "product"  => $product
+        ]);
+    }
+}
